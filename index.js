e2f1b78d9f414008b8a6e6b9e3d5a0acc082735d
Mamta kushawaha <159121000+mamatakushawaha@users.noreply.github.com>
2024-07-26 12:44:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+function check(){
+    var input=document.getElementById('fname');
+    if(input.value !=" "){
+        ecertificate();
+    }
+    else{
+        alert("input field empty");
+    }
+}
+function ecertificate(){
+    var x= document.getElementById("main");
+    x.style.display="block";
+    var cname=document.getElementById("fname").value;
+    var courses=document.getElementById("fcourse").value;
+    document.getElementsByTagName('B')[0].innerHTML=cname;
+    document.getElementsByTagName('h3')[0].innerHTML=courses;
+}
